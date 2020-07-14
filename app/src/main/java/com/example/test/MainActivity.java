b0a8faf3533aeb51297c1858d8a11abc45798d86
Wu <1349482803@qq.com>
2020-07-14 18:49:39 +0800

    4. load module

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void fun(){
-
+        //test
 
     }
     public void login(){
